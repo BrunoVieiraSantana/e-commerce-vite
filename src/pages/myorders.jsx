2cef32e5fd2929538d29651e8dfa742a3eb67deb
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-02-22 15:57:49 -0300

    commit 31

--- src/pages/myorders.jsx
+++ src/pages/myorders.jsx
@@ -13,38 +13,7 @@ const MyOrders = () => {
   return (
     <CartProvider> 
       <div>
-        <div className="flex flex-col">
-          <div className="bg-white">
-          </div>
-          <div className="flex flex-row justify-between">
-            <div className="ml-4">
-              <img className="h-[80px]" src="images/card.png" alt=""></img>
-              <h1 className="text-blue-900 font-bold text-2xl">
-                {product.title}
-              </h1>
-              <p className="text-neutral-700 text-lg">{product.description}</p>
-              <span className="text-orange-400 font-semibold text-2xl">R${product.currentPrice.toFixed(2)}</span>
-              <AddToCart
-                name={product.title}
-                price={product.currentPrice}
-                thumbnail={product.images.thumbnails[0].src}
-              />
-            </div>
-            <div className="mr-4">
-              <img className="h-[80px]" src="images/card.png" alt=""></img>
-              <h1 className="text-blue-900 font-bold text-2xl">
-                {product.title}
-              </h1>
-              <p className="text-neutral-700 text-lg">{product.description}</p>
-              <span className="text-orange-400 font-semibold text-2xl">R${product.currentPrice.toFixed(2)}</span>
-              <AddToCart
-                name={product.title}
-                price={product.currentPrice}
-                thumbnail={product.images.thumbnails[0].src}
-              />
-            </div>
-          </div>
-        </div>
+        Meus Pedidos
       </div>
     </CartProvider>
   );
