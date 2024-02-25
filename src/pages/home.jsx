bac9e82effabd5aefd5952937e8bcc7c9f795f9f
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-02-25 17:02:03 -0300

    commit 50

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,56 +1,66 @@
+import { useState, useEffect } from 'react';
 import styles from "../page.module.css";
 import cardStyles from './card.module.css';
 import utils from "../components/utils.module.css";
 import AddToCart from "../components/addToCart";
-import { product } from "../../public/data";
-import { Link, useNavigate } from "react-router-dom"
-import { useContext } from "react"
-
-
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch('https://e-commerce-api-bay.vercel.app/api/v1/products');
+        if (!response.ok) {
+          throw new Error('Failed to fetch products');
+        }
+        const data = await response.json();
+        setProducts(data.data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
   return (
-    
-    
     <div>
-      <div> 
-          <img className={styles.rectanglehomelarge} src="images/rectangle-home-large.png" alt="Rectangle Home Large"/>
+      <div>
+        <img className={styles.rectanglehomelarge} src="images/rectangle-home-large.png" alt="Rectangle Home Large"/>
       </div>
 
       <div>
-          <img className={styles.rectanglehome} src="images/rectangle-home.jpg" alt="Rectangle Home"/>
+        <img className={styles.rectanglehome} src="images/rectangle-home.jpg" alt="Rectangle Home"/>
       </div>
 
       <a href="/myorders" className={styles.homebutton}>Aproveite a Oferta</a>
 
-    <div className={`${styles.main} ${utils.flex}`}>
-
-      <div className={styles.productDetail}>
-      
-        <span
-          className={`${styles.companyName} ${utils.upperCase} ${utils.textOrange400} ${utils.fs300} ${utils.fw700}`}
-        >
-          Destaques
+      <div className={`${styles.main} ${utils.flex}`}>
+        <div className={styles.productDetail}>
+          <span className={`${styles.companyName} ${utils.upperCase} ${utils.textOrange400} ${utils.fs300} ${utils.fw700}`}>
+            Destaques
           </span>
           <div className={cardStyles.cardContainer}>
-          {product.map((item) => (
-            <div key={item.id} className={cardStyles.card}>
-              <img src={item.images.mainImgs[0]} className={cardStyles.img} /> 
-              <div className={cardStyles.content}>
-                <h1>{item.title}</h1>
-                <p>{item.description}</p> 
-                <span>R${item.currentPrice.toFixed(2)}</span> 
-                <AddToCart
-                name={item.title}
-                price={item.currentPrice}
-                thumbnail={item.images.thumbnails[0]} 
-              />
+            {products.map((item) => (
+              <div key={item.id_product} className={cardStyles.card}>
+                <img src={item.mainimg} className={cardStyles.img} alt={item.title}/> 
+                <div className={cardStyles.content}>
+                  <h1>{item.title}</h1>
+                  <p>{item.descript}</p> 
+                  <span>R${item.currentprice.toFixed(2)}</span> 
+                  <AddToCart
+                    name={item.title}
+                    price={item.currentprice}
+                    thumbnail={item.thumbnail} 
+                  />
+                </div>
               </div>
-            </div>
-      ))}
+            ))}
+          </div>
+        </div>
       </div>
-      </div>
-    </div>
     </div>
   );
 }
